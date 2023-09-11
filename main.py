e6000708b406122efec63800f8d217be9633852e
Eli <eli.340@gmail.com>
2023-09-11 14:11:20 -0400

    1

--- main.py
+++ main.py
@@ -1,16 +1,52 @@
-# This is a sample Python script.
+# streamlit_app.py
+import os
+import shutil
+import streamlit as st
+def move_files(source_folder, dest_folder):
+    for filename in os.listdir(source_folder):
+        source_path = os.path.join(source_folder, filename)
+        dest_path = os.path.join(dest_folder, filename)
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+        if os.path.isfile(source_path):
+            shutil.move(source_path, dest_path)
 
+def check_password():
+    """Returns `True` if the user had a correct password."""
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if (
+            st.session_state["username"] in st.secrets["passwords"]
+            and st.session_state["password"]
+            == st.secrets["passwords"][st.session_state["username"]]
+        ):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # don't store username + password
+            del st.session_state["username"]
+        else:
+            st.session_state["password_correct"] = False
 
+    if "password_correct" not in st.session_state:
+        # First run, show inputs for username + password.
+        st.text_input("Username", key="username")
+        st.text_input(
+            "Password", type="password", on_change=password_entered, key="password"
+        )
+        return False
+    elif not st.session_state["password_correct"]:
+        # Password not correct, show input + error.
+        st.text_input("Username", key="username")
+        st.text_input(
+            "Password", type="password", on_change=password_entered, key="password"
+        )
+        st.error("😕 User not known or password incorrect")
+        return False
+    else:
+        # Password correct.
+        move_files('pages/hide', 'pages')
+        return True
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+move_files('pages', 'pages/hide')
+if check_password():
+    st.write("Here goes your normal Streamlit app...")
+    st.button("Click me")
